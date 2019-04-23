4561243d74eacdaa80d9aa694512b0149a8c11a9
Klordok <blargsmarg@gmail.com>
2019-04-22 18:49:20 -0700

    created repository and New_Years_Chaos

---
+++ New_Year_Chaos.py
@@ -0,0 +1,10 @@
+""" 
+Function Description
+
+Complete the function minimumBribes in the editor below. It must print an integer representing the minimum number of bribes necessary, or Too chaotic if the line configuration is not possible.
+
+minimumBribes has the following parameter(s):
+
+    q: an array of integers
+"""
+
